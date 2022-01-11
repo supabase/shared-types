c086eafebe3513a390e2a1b086f556f80f08a852
Div Arora <root@darora.com>
2022-01-11 17:28:10 +0800

    chore: add type for update supabase api key task

--- src/projects.ts
+++ src/projects.ts
@@ -1,9 +1,14 @@
 export enum ProjectTask {
   UpdateJwt = 'update_jwt',
+  UpdateSupabaseApiKey = 'update_supabase_api_key',
 }
 
 export interface UpdateJwtSecretPayload {
   user_id: number
   project_ref: string
   new_jwt_secret_encrypted: string
+}
+
+export interface UpdateSupabaseApiKeyPayload {
+  project_id: number
 }
