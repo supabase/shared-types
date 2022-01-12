eba1a306d3f94be976827d0bc8f93c2733ae83d6
Div Arora <root@darora.com>
2022-01-12 14:15:38 +0800

    chore: additional fields for JWT updates

--- src/projects.ts
+++ src/projects.ts
@@ -7,8 +7,10 @@ export interface UpdateJwtSecretPayload {
   user_id: number
   project_ref: string
   new_jwt_secret_encrypted: string
+  change_tracking_id: string
 }
 
 export interface UpdateSupabaseApiKeyPayload {
   project_id: number
+  next_api_key_supabase_encrypted: string
 }
