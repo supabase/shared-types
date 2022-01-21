fde396093a0be3170aab39c1525eda8b514faa64
Div Arora <root@darora.com>
2022-01-21 15:49:59 +0800

    chore: types for project creation requests

--- src/projects.ts
+++ src/projects.ts
@@ -14,3 +14,30 @@ export interface UpdateSupabaseApiKeyPayload {
   project_id: number
   next_api_key_supabase_encrypted: string
 }
+
+export enum InitializationPayloadVersion {
+  Version1 = 1,
+}
+
+export enum InitializationType {
+  NewProject = 'new_project',
+  Restore = 'restore',
+}
+
+export interface ProjectInitializationBasePayload {
+  anon_key_encrypted: string
+  api_id: string
+  initialization_type: InitializationType
+  payload_version: InitializationPayloadVersion
+  project_id: number
+  service_key_encrypted: string
+}
+
+export interface NewProjectInitializationPayload extends ProjectInitializationBasePayload {
+  auth_site_url: string
+  db_sql: string
+}
+
+export interface RestoreProjectPayload extends ProjectInitializationBasePayload {
+  backup_id: number
+}
