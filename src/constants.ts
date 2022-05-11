989994ce1b791ad9c6210315ab3da60186b426af
Beng Eu <beng@bengeu.com>
2022-05-11 14:52:01 +0800

    chore: add permission action enum for ABAC (#13)

--- src/constants.ts
+++ src/constants.ts
@@ -11,3 +11,27 @@ export enum ServiceNames {
 }
 
 export const DEFAULT_REALTIME_SLOT_NAME_WITH_WALRUS = 'realtime_rls'
+
+export enum PermissionAction {
+  ANALYTICS_READ = 'analytics:Read',
+  AUTH_EXECUTE = 'auth:Execute',
+  BILLING_READ = 'billing:Read',
+  BILLING_WRITE = 'billing:Write',
+  FUNCTIONS_READ = 'functions:Read',
+  FUNCTIONS_WRITE = 'functions:Write',
+  INFRA_EXECUTE = 'infra:Execute',
+  SQL_SELECT = 'sql:Read:Select',
+  SQL_DELETE = 'sql:Write:Delete',
+  SQL_INSERT = 'sql:Write:Insert',
+  SQL_UPDATE = 'sql:Write:Update',
+  STORAGE_ADMIN_READ = 'storage:Admin:Read',
+  STORAGE_ADMIN_WRITE = 'storage:Admin:Write',
+  TENANT_SQL_ADMIN_READ = 'tenant:Sql:Admin:Read',
+  TENANT_SQL_ADMIN_WRITE = 'tenant:Sql:Admin:Write',
+  TENANT_SQL_CREATE_TABLE = 'tenant:Sql:CreateTable',
+  TENANT_SQL_DELETE = 'tenant:Sql:Write:Delete',
+  TENANT_SQL_INSERT = 'tenant:Sql:Write:Insert',
+  TENANT_SQL_QUERY = 'tenant:Sql:Query',
+  TENANT_SQL_SELECT = 'tenant:Sql:Read:Select',
+  TENANT_SQL_UPDATE = 'tenant:Sql:Write:Update',
+}
